c3ca0de15fb0a6883b9b47a312c3844f6da9bdc3
Ravi kumar <raviraj7301325@gmail.com>
2025-09-18 12:13:54 +0000

    Datesin

---
+++ javascript/Datesin.js
@@ -0,0 +1,30 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2025, 2, 10)
+// console.log(myCreateDate.toLocaleString());
+
+
+// let myCreateDate = new Date("2025-01-25")
+// console.log(myCreateDate.toLocaleString());
+
+
+
+// let myTimeStamp = Date.now
+
+// console.log(myTimeStamp);
+
+// console.log(myCreatedDate.getTime());
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+  
+})
