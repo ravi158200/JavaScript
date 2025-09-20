5d5d95507fd73ae58f3d9221ef2a7e5c3c7d87d0
Ravi kumar <raviraj7301325@gmail.com>
2025-09-20 16:46:22 +0000

    control flow code

---
+++ javascript3/iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoke Function Expressions (IIFE)
+
+// function chai (){
+//     console.log("DB CONNECTED")
+// }
+// chai()
+
+(function chai (){
+    console.log("DB CONNECTED")
+})();
+
+( (name) => {
+    console.log('DB CONNECTED TWO ${name}');
+})('Ravi')
